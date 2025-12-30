1de4e1dc69de7a828e359ba78532dae3e7614aa4
Rakesh Naskar <rakeshnaskar499@gmail.com>
2025-12-30 14:24:49 +0530

    all final file add

---
+++ front_java.js
@@ -0,0 +1,31 @@
+const button = document.getElementById("submitBtn");
+const buffer = document.getElementById("buffer");
+const result = document.getElementById("result");
+
+button.addEventListener("click", () => {
+  // Reset
+  result.innerText = "";
+  buffer.classList.remove("hidden");
+
+  button.disabled = true;
+  button.innerText = "Calculating 🤯";
+
+  // Buffer delay (3 seconds)
+  setTimeout(() => {
+    const randomNumber = Math.floor(Math.random() * 51) + 50;
+
+    buffer.classList.add("hidden");
+    if (randomNumber > 80) {
+      result.innerText = `🚑 Mental Score ${randomNumber}  
+মাথা আছে, কিন্তু warranty শেষ 🤡
+`;
+    } else {
+      result.innerText = `🎉 অভিনন্দন!  
+Mental Score ${randomNumber}, মাথা আজ কাজ করছে 🤪
+`;
+    }
+
+    button.disabled = false;
+    button.innerText = "Submit";
+  }, 3000);
+});
